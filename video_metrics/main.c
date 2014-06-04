36807c9b2ce02cd23139d467c85290521350be73
Alberto Vigata <alberto@nelalabs.com>
2014-06-04 16:51:01 -0700

    Initial Commit

---
+++ video_metrics/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  video_metrics
+//
+//  Created by Alberto Vigata on 6/4/14.
+//  Copyright (c) 2014 Alberto Vigata. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
